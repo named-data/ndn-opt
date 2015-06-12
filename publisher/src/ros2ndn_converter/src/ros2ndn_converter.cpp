8a3c707f620ac5ee3b12d4273289f8078a08a3e0
Alex Nano <nanodust@gmail.com>
2015-06-12 13:56:52 -0700

    auto-restart: does not recreate face, track publisher and hint publisher on restart

--- publisher/src/ros2ndn_converter/src/ros2ndn_converter.cpp
+++ publisher/src/ros2ndn_converter/src/ros2ndn_converter.cpp
@@ -49,58 +49,59 @@ main(int argc, char **argv)
   nh.param("json/spacing", jsonParameters.jsonSpacing, false);
   nh.param("json/use_tabs", jsonParameters.jsonUseTabs, false);
   
+  ndn::NdnController::Parameters ncParams;
+  ncParams.nh = nh;
+
+  std::string prefix, node_name;
+  
+  // setting up NdnController  
+  nh.param("ndn/prefix", prefix, std::string("/ndn/edu/ucla/remap"));
+  nh.param("ndn/node_name", node_name, std::string("node0"));
+  ncParams.prefix = ndn::NdnController::getInstancePrefix(prefix, node_name);
+  nh.param("ndn/segment_length", ncParams.segmentLength, 2048);
+  nh.param("ndn/daemon_host", ncParams.host, std::string("localhost"));
+  nh.param("ndn/deamon_port", ncParams.port, 6363);
+
+  ndn::NdnController ndnController(ncParams);
+  
+  ndn::opt::ActiveTracks::Parameters atParams;
+  atParams.jsonParameters = jsonParameters;
+
+  ndn::opt::ActiveTracks activeTracks(atParams);
+
+  // setting up Track Hint Publisher
+  ndn::opt::TrackHintPublisher::Parameters thpParams;
+  nh.param("hints/rate", thpParams.rate, 5);
+  nh.param("hints/freshness_period", thpParams.freshnessPeriod, 200);
+  thpParams.nh = nh;
+  thpParams.ndnController = &ndnController;
+  thpParams.activeTracks = &activeTracks;
+      
+  ndn::opt::TrackHintPublisher trackHintPublisher(thpParams);
+
+  // setting up Track Publisher
+  ndn::opt::TrackPublisher::Parameters  tpParams;
+  nh.param("ndn/freshness_period", tpParams.freshnessPeriod, 5000);
+  tpParams.bufferLength = 2048;
+  tpParams.nh = ncParams.nh;
+  tpParams.jsonParameters = jsonParameters;
+  tpParams.activeTracks = &activeTracks;
+  tpParams.ndnController = &ndnController; 
+      
+  ndn::opt::TrackPublisher trackPublisher(tpParams);  
+
   // Zhehao: put the publisher in a loop until sigint is caught, or other signals
-  while (true) {
+  while (true) { 
+    // Zhehao: set up a manual exception after 5 seconds for testing purposes;       
+    //ndnController.scheduleException(3);
+  
     try 
     {
-      // setting up NdnController
-      ndn::NdnController::Parameters ncParams;
-      ncParams.nh = nh;
-
-      std::string prefix, node_name;
-  
-      nh.param("ndn/prefix", prefix, std::string("/ndn/edu/ucla/remap"));
-      nh.param("ndn/node_name", node_name, std::string("node0"));
-      ncParams.prefix = ndn::NdnController::getInstancePrefix(prefix, node_name);
-      nh.param("ndn/segment_length", ncParams.segmentLength, 2048);
-      nh.param("ndn/daemon_host", ncParams.host, std::string("localhost"));
-      nh.param("ndn/deamon_port", ncParams.port, 6363);
-
-      ndn::NdnController ndnController(ncParams);
-      
-      // Zhehao: set up a manual exception after 5 seconds for testing purposes; this does not yet work as expected...
-      //ndnController.scheduleException(3);
-      
       if (0 == ndnController.startNdnProcessing())
       {
         // Zhehao: test exception catching
         //std::string("abc").substr(10);
         // setting up Active Tracks structure
-        ndn::opt::ActiveTracks::Parameters atParams;
-        atParams.jsonParameters = jsonParameters;
-
-        ndn::opt::ActiveTracks activeTracks(atParams);
-
-        // setting up Track Hint Publisher
-        ndn::opt::TrackHintPublisher::Parameters thpParams;
-        nh.param("hints/rate", thpParams.rate, 5);
-        nh.param("hints/freshness_period", thpParams.freshnessPeriod, 200);
-        thpParams.nh = nh;
-        thpParams.ndnController = &ndnController;
-        thpParams.activeTracks = &activeTracks;
-      
-        ndn::opt::TrackHintPublisher trackHintPublisher(thpParams);
-
-        // setting up Track Publisher
-        ndn::opt::TrackPublisher::Parameters  tpParams;
-        nh.param("ndn/freshness_period", tpParams.freshnessPeriod, 5000);
-        tpParams.bufferLength = 2048;
-        tpParams.nh = ncParams.nh;
-        tpParams.jsonParameters = jsonParameters;
-        tpParams.activeTracks = &activeTracks;
-        tpParams.ndnController = &ndnController; 
-	
-        ndn::opt::TrackPublisher trackPublisher(tpParams);
 	
         trackPublisher.subscribe("input_topic");
         trackHintPublisher.start();
@@ -116,6 +117,9 @@ main(int argc, char **argv)
     catch (std::exception& exception) 
     {
       ROS_ERROR_STREAM("got exception: " << exception.what());
+      trackHintPublisher.stop();
+      trackPublisher.unsubscribe();
+      ndnController.stopNdnProcessing(); 
     }
     ros::Duration(1).sleep();
     std::cout << "Relaunch ndn publisher because of caught exception" << std::endl;
